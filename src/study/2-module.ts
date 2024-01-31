ab45fb9a58beefc700f28aa069d1e7086ef62363
Oleksii Khomiakov <alex.xag1987@gmail.com>
2024-01-31 10:02:07 +0200

    task 7

--- src/study/2-module.ts
+++ src/study/2-module.ts
@@ -47,3 +47,40 @@ const bookDetails: MixedType = {
 };
 
 // Generics
+// 1. Create commune functions, classes that can work whith different entry types and save the typisation
+
+// function identity<T>(arg: T): T {
+//   return arg;
+// }
+
+// // 1
+// // function identity<string>(arg:string): string {
+// //   return arg;
+// // }
+
+// // 2
+// // function identity<number>(arg:number): number {
+// //   return arg;
+// // }
+
+// let output1 = identity<string>("myString");
+// let output2 = identity<number>(100);
+
+// //////////////////////////// Task for students
+//  Create function reberse, that take an array with any type and return array in reverse-row
+
+function reverse<T>(items: T[]): T[] {
+  return items.reverse();
+}
+
+let numbers = reverse<number>([1, 2, 3, 4, 5]);
+console.log(numbers); // [5,4,3,2,1]
+
+let strings = reverse<string>(["a", "b", "c", "d"]);
+console.log(strings); // ['d','c','b','a']
+
+// extends and keyOf
+
+function lengthOfObject<T extends { length: number }>(obj: T): number {
+  return obj.length;
+}
