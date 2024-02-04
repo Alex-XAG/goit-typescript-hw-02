93d5f17c969fe59291230851fe9023c3117a0903
Oleksii Khomiakov <alex.xag1987@gmail.com>
2024-02-04 15:19:27 +0200

    8,9 tasks is done

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,22 +2,36 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+type Details = {
+  [key: string]: Date;
+};
+// type PageDiscription = {
+//   [key: string]: string | number | string[] | Details; // any
+// };
 
-const page2 = {
-  title: 'Python or Js',
+type PageDiscription = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: Details;
+};
+const page1: PageDiscription = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2: PageDiscription = {
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
